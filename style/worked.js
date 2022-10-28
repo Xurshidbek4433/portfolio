675a3d9ace0a6edc0610930df1250d73197518bd
Xurshidbek3344 <xurshidbekxusanboyev3344@gmail.com>
2022-10-28 22:51:34 +0300

    Nomini ozgartirish

---
+++ style/worked.js
@@ -0,0 +1,25 @@
+// const sinov = document.getElementById('left__link2')
+
+
+// sinov.addEventListener('click' ()  => {
+//     document.getElementById('parag').inner
+// } )
+
+
+// document.getElementById('tugma').addEventListener('click', () => {
+//     document.getElementById('parag').innerText = 'Ozgardi 😄';
+//     document.getElementById('parag1').innerText = 'Boshqa 😎';
+//   });
+
+document.getElementById('left__link2').addEventListener('click', () => {
+    document.getElementById('parag').innerText = 'ozgardi';
+    document.getElementById('item__text').innerText = 'oktyabr'
+});
+document.getElementById('left__link3').addEventListener('click', () => {
+    document.getElementById('parag').innerText = 'iloji bor';
+    document.getElementById('item__text').innerText = 'sentabr'
+});
+
+document.getElementById('left__link1').addEventListener('click',  () => {
+    document.getElementById('parag').innerText = 'Software Engineer @WezaProsoft'
+} )
