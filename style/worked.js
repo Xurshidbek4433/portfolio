b5dc70de13a06d869628ce78aec26b140cbf5a1d
Xurshidbek3344 <xurshidbekxusanboyev3344@gmail.com>
2022-10-31 22:59:06 +0300

    animatsiyadan foydalangan xolda davom etyapmiz

--- style/worked.js
+++ style/worked.js
@@ -1,25 +1,35 @@
 // const sinov = document.getElementById('left__link2')
 
-
 // sinov.addEventListener('click' ()  => {
 //     document.getElementById('parag').inner
 // } )
-
 
 // document.getElementById('tugma').addEventListener('click', () => {
 //     document.getElementById('parag').innerText = 'Ozgardi 😄';
 //     document.getElementById('parag1').innerText = 'Boshqa 😎';
 //   });
 
-document.getElementById('left__link2').addEventListener('click', () => {
-    document.getElementById('parag').innerText = 'ozgardi';
-    document.getElementById('item__text').innerText = 'oktyabr'
+document.getElementById("left__link2").addEventListener("click", () => {
+  document.getElementById("parag").innerText = "ozgardi";
+  document.getElementById("item__text").innerText = "oktyabr";
 });
-document.getElementById('left__link3').addEventListener('click', () => {
-    document.getElementById('parag').innerText = 'iloji bor';
-    document.getElementById('item__text').innerText = 'sentabr'
+document.getElementById("left__link3").addEventListener("click", () => {
+  document.getElementById("parag").innerText = "iloji bor";
+  document.getElementById("item__text").innerText = "sentabr";
 });
 
-document.getElementById('left__link1').addEventListener('click',  () => {
-    document.getElementById('parag').innerText = 'Software Engineer @WezaProsoft'
-} )
+document.getElementById("left__link1").addEventListener("click", () => {
+  document.getElementById("parag").innerText = "Software Engineer @WezaProsoft";
+});
+
+// const skils = document.getElementsByClassName(public__slick-inner)[0]
+
+document.getElementById("right").addEventListener("click", test);
+
+console.log(function test() {
+  document.getElementsByClassName("public__slick-inner").scrollRight += 100;
+  
+}
+
+
+);
